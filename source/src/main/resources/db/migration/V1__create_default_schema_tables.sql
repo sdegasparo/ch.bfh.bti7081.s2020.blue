68c1cad9bcb56525323f52b262627d3ac16241de
Timon Borter <timon.borter@gmx.ch>
2020-05-08 10:00:16 +0200

    debug migration V1 script. remove trailing commas and add semicolons after statements.

--- source/src/main/resources/db/migration/V1__create_default_schema_tables.sql
+++ source/src/main/resources/db/migration/V1__create_default_schema_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE patient (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     surname VARCHAR(128) NOT NULL,
-    given_name VARCHAR(128) NOT NULL,
-)
+    given_name VARCHAR(128) NOT NULL
+);
 
 CREATE TABLE login (
     username VARCHAR(32) PRIMARY KEY NOT NULL,
@@ -11,15 +11,15 @@ CREATE TABLE login (
     is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
     is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
     patient_id BIGINT NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient(id),
-)
+    FOREIGN KEY (patient_id) REFERENCES patient(id)
+);
 
 CREATE TABLE challenge (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(64) NOT NULL,
     content TEXT NOT NULL,
-    criteria TEXT NOT NULL,
-)
+    criteria TEXT NOT NULL
+);
 
 CREATE TABLE patient_challenges (
     patient_id BIGINT NOT NULL,
@@ -27,37 +27,37 @@ CREATE TABLE patient_challenges (
     completed BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (patient_id, challenge_id),
     FOREIGN KEY (patient_id) REFERENCES patient(id),
-    FOREIGN KEY (challenge_id) REFERENCES challenge(id),
-)
+    FOREIGN KEY (challenge_id) REFERENCES challenge(id)
+);
 
 CREATE TABLE journal_entry (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     creation_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()::timestamp,
     title VARCHAR(64) NOT NULL,
-    content TEXT NOT NULL,
-)
+    content TEXT NOT NULL
+);
 
 CREATE TABLE patient_journal_entry (
     patient_id BIGINT NOT NULL,
     journal_entry_id BIGINT NOT NULL,
-    PRIMARY KEY (patient_id, journal_id),
+    PRIMARY KEY (patient_id, journal_entry_id),
     FOREIGN KEY (patient_id) REFERENCES patient(id),
-    FOREIGN KEY (journal_entry_id) REFERENCES journal_entry(id),
-)
+    FOREIGN KEY (journal_entry_id) REFERENCES journal_entry(id)
+);
 
 CREATE TABLE achievement (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(64) NOT NULL,
-    description VARCHAR(256) NOT NULL,
-)
+    description VARCHAR(256) NOT NULL
+);
 
 CREATE TABLE patient_achieved (
     patient_id BIGINT NOT NULL,
     achievement_id BIGINT NOT NULL,
-    PRIMARY KEY (patient_id, journal_id),
+    PRIMARY KEY (patient_id, achievement_id),
     FOREIGN KEY (patient_id) REFERENCES patient(id),
-    FOREIGN KEY (achievement_id) REFERENCES achievement(id),
-)
+    FOREIGN KEY (achievement_id) REFERENCES achievement(id)
+);
 
 CREATE TABLE reward (
     id BIGSERIAL PRIMARY KEY NOT NULL,
@@ -65,16 +65,16 @@ CREATE TABLE reward (
     description TEXT NOT NULL,
     points SMALLINT NOT NULL,
     achievement_id BIGINT,
-    FOREIGN KEY (achievement_id) REFERENCES achievement(id),
-)
+    FOREIGN KEY (achievement_id) REFERENCES achievement(id)
+);
 
 CREATE TABLE challenge_rewards (
     challenge_id BIGINT NOT NULL,
     reward_id BIGINT NOT NULL,
     PRIMARY KEY (challenge_id, reward_id),
     FOREIGN KEY (challenge_id) REFERENCES challenge(id),
-    FOREIGN KEY (reward_id) REFERENCES reward(id),
-)
+    FOREIGN KEY (reward_id) REFERENCES reward(id)
+);
 
 CREATE TABLE therapist (
     title VARCHAR(128) NOT NULL,
@@ -82,5 +82,5 @@ CREATE TABLE therapist (
     given_name VARCHAR(128) NOT NULL,
     street VARCHAR(256) NOT NULL,
     place VARCHAR(265) NOT NULL,
-    information TEXT NOT NULL,
-)
+    information TEXT NOT NULL
+);
