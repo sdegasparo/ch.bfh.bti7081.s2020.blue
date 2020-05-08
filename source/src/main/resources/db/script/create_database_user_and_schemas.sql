4d04c31af3719128514f43372de844207c6da41b
Timon Borter <timon.borter@gmx.ch>
2020-05-08 10:54:40 +0200

    add sql scripts for database users and schema.

---
+++ source/src/main/resources/db/script/create_database_user_and_schemas.sql
@@ -0,0 +1,7 @@
+CREATE USER social_anxiety WITH PASSWORD 'social_anxiety_password';
+
+CREATE DATABASE social_anxiety;
+GRANT ALL PRIVILEGES ON DATABASE social_anxiety TO social_anxiety;
+
+CREATE DATABASE social_anxiety_integration;
+GRANT ALL PRIVILEGES ON DATABASE social_anxiety_integration TO social_anxiety;
