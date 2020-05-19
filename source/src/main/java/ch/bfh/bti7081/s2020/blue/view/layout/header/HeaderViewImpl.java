80df3dcf1ce1b2282a0b52a9893aef527e701b2b
Sven De Gasparo <degas1@bfh.ch>
2020-05-19 18:31:17 +0200

    Style Header

--- source/src/main/java/ch/bfh/bti7081/s2020/blue/view/layout/header/HeaderViewImpl.java
+++ source/src/main/java/ch/bfh/bti7081/s2020/blue/view/layout/header/HeaderViewImpl.java
@@ -5,8 +5,6 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
 
 public class HeaderViewImpl extends HorizontalLayout implements HeaderView {
 
@@ -18,22 +16,44 @@ public class HeaderViewImpl extends HorizontalLayout implements HeaderView {
     listener = new HeaderPresenter(this);
     listener.onInit();
 
-    H1 title = new H1("Social Anxiety Application");
+    HorizontalLayout titleLayout = new HorizontalLayout();
+    titleLayout.setWidth("33.33%");
+    HorizontalLayout navLayout = new HorizontalLayout();
+    navLayout.setWidth("33.33%");
+    HorizontalLayout userLayout = new HorizontalLayout();
+    userLayout.setWidth("33.33%");
 
+    H1 title = new H1("Social Anxiety Application");
+    titleLayout.add(title);
+    titleLayout.setAlignItems(Alignment.END);
+
+    /*
     Tab tab1 = new Tab(new Anchor("/", "Home"));
     Tab tab2 = new Tab(new Anchor("challenge", "Challenge"));
     Tab tab3 = new Tab(new Anchor("journal", "Journal"));
     Tab tab4 = new Tab(new Anchor("therapists", "Therapist"));
-    Tabs tabs = new Tabs(tab1, tab2, tab3, tab4);
+    Tabs tabs = new Tabs(tab1, tab2, tab3, tab4);*/
+
+    Anchor anchor1 = new Anchor("challenge", "Challenge");
+    Anchor anchor2 = new Anchor("journal", "Journal");
+    Anchor anchor3 = new Anchor("therapists", "Therapist");
+    navLayout.add(anchor1, anchor2, anchor3);
+    navLayout.setAlignItems(Alignment.CENTER);
+    navLayout.setSpacing(true);
 
     Text currentUser = new Text("Current User"); // TODO: Create Methode to getCurrentUser
+    userLayout.add(currentUser);
 
-    layout.add(title, tabs, currentUser);
+    layout.add(titleLayout, navLayout, userLayout);
+    layout.setWidth("100%");
+    layout.setAlignItems(Alignment.BASELINE);
 
   }
 
   @Override
   public void display() {
     add(layout);
+    setWidth("100%");
+    setAlignItems(Alignment.BASELINE);
   }
 }
