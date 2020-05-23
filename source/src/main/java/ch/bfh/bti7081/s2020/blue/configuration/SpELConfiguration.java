47ae6fdec234a7cb8fb5e45c00dfaa30bbec1ff5
Timon Borter <timon.borter@gmx.ch>
2020-05-23 13:44:18 +0200

    fix spelling error in SpEL configuration class.

--- source/src/main/java/ch/bfh/bti7081/s2020/blue/configuration/SpELConfiguration.java
+++ source/src/main/java/ch/bfh/bti7081/s2020/blue/configuration/SpELConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 
 @Configuration
-public class SpELCOnfiguration {
+public class SpELConfiguration {
 
   @Bean
   public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
