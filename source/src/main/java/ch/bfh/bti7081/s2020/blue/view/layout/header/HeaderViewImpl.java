5c75844fa00f0d46b02db7fd1e6955c4f69d7e65
Sven De Gasparo <degas1@bfh.ch>
2020-05-23 14:11:18 +0200

    add link to title, get username and some styling

--- source/src/main/java/ch/bfh/bti7081/s2020/blue/view/layout/header/HeaderViewImpl.java
+++ source/src/main/java/ch/bfh/bti7081/s2020/blue/view/layout/header/HeaderViewImpl.java
@@ -1,10 +1,11 @@
 package ch.bfh.bti7081.s2020.blue.view.layout.header;
 
 import ch.bfh.bti7081.s2020.blue.presenter.HeaderPresenter;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class HeaderViewImpl extends HorizontalLayout implements HeaderView {
 
@@ -23,37 +24,42 @@ public class HeaderViewImpl extends HorizontalLayout implements HeaderView {
     HorizontalLayout userLayout = new HorizontalLayout();
     userLayout.setWidth("33.33%");
 
-    H1 title = new H1("Social Anxiety Application");
+    Anchor title = new Anchor("home", "Social Anxiety Application");
+    title.getStyle().set("color", "black");
     titleLayout.add(title);
-    titleLayout.setAlignItems(Alignment.END);
-
-    /*
-    Tab tab1 = new Tab(new Anchor("/", "Home"));
-    Tab tab2 = new Tab(new Anchor("challenge", "Challenge"));
-    Tab tab3 = new Tab(new Anchor("journal", "Journal"));
-    Tab tab4 = new Tab(new Anchor("therapists", "Therapist"));
-    Tabs tabs = new Tabs(tab1, tab2, tab3, tab4);*/
 
     Anchor anchor1 = new Anchor("challenge", "Challenge");
     Anchor anchor2 = new Anchor("journal", "Journal");
     Anchor anchor3 = new Anchor("therapists", "Therapist");
-    navLayout.add(anchor1, anchor2, anchor3);
-    navLayout.setAlignItems(Alignment.CENTER);
-    navLayout.setSpacing(true);
 
-    Text currentUser = new Text("Current User"); // TODO: Create Methode to getCurrentUser
+    anchor1.getStyle().set("color", "black");
+    anchor1.getStyle().set("margin", "0px");
+    anchor2.getStyle().set("color", "black");
+    anchor2.getStyle().set("margin", "0px");
+    anchor3.getStyle().set("color", "black");
+    anchor3.getStyle().set("margin", "0px");
+    navLayout.add(anchor1, anchor2, anchor3);
+    navLayout.setSpacing(true);
+    navLayout.setJustifyContentMode(JustifyContentMode.AROUND);
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Label currentUser = new Label(
+        authentication.getName());
     userLayout.add(currentUser);
+    userLayout.setJustifyContentMode(JustifyContentMode.END);
 
     layout.add(titleLayout, navLayout, userLayout);
     layout.setWidth("100%");
-    layout.setAlignItems(Alignment.BASELINE);
-
+    layout.setHeight("75px");
+    layout.getStyle().set("font-size", "1.5em");
+    layout.getStyle().set("border-bottom", "3px solid #000000");
   }
 
   @Override
   public void display() {
     add(layout);
     setWidth("100%");
-    setAlignItems(Alignment.BASELINE);
+    setMargin(false);
+    setPadding(false);
   }
 }
