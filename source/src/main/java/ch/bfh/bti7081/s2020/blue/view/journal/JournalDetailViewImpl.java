62f50170fc5dac1539fc10965a2881b1a2902dba
calesanz <8714917+calesanz@users.noreply.github.com>
2020-06-07 21:13:38 +0200

    Add delete confirmation dialog

--- source/src/main/java/ch/bfh/bti7081/s2020/blue/view/journal/JournalDetailViewImpl.java
+++ source/src/main/java/ch/bfh/bti7081/s2020/blue/view/journal/JournalDetailViewImpl.java
@@ -6,8 +6,10 @@ import ch.bfh.bti7081.s2020.blue.util.BeanInjector;
 import ch.bfh.bti7081.s2020.blue.view.HomeView;
 import ch.bfh.bti7081.s2020.blue.view.layout.SocialAnxietyLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.BeforeEvent;
@@ -15,10 +17,12 @@ import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 
 @Route("journal")
-public class JournalDetailViewImpl extends SocialAnxietyLayout implements JournalDetailView, HasUrlParameter<Long> {
+public class JournalDetailViewImpl extends SocialAnxietyLayout implements JournalDetailView,
+    HasUrlParameter<Long> {
 
   private JournalDetailListener listener;
   private Div content;
+  private Dialog deleteConfirmationDialog = new Dialog();
 
   public JournalDetailViewImpl(BeanInjector beanInjector) {
     super(beanInjector);
@@ -44,6 +48,28 @@ public class JournalDetailViewImpl extends SocialAnxietyLayout implements Journa
     getUI().ifPresent(ui -> ui.navigate(HomeView.class));
   }
 
+  @Override
+  public void showDeleteConfirmationDialog() {
+    deleteConfirmationDialog = new Dialog();
+
+    Label messageLabel = new Label();
+    messageLabel.setText("Löschen?");
+
+    Button confirmButton = new Button("Confirm", event ->
+        listener.onJournalEntryDeleteConfirm()
+    );
+    Button cancelButton = new Button("Cancel",
+        event -> listener.onJournalEntryDeleteCancel());
+
+    deleteConfirmationDialog.add(messageLabel, confirmButton, cancelButton);
+    deleteConfirmationDialog.open();
+  }
+
+  @Override
+  public void hideDeleteConfirmationDialog() {
+    deleteConfirmationDialog.close();
+  }
+
   private FormLayout createFormLayout(Binder<JournalEntryDto> binder) {
     FormLayout formlayout = new FormLayout();
 
@@ -65,6 +91,7 @@ public class JournalDetailViewImpl extends SocialAnxietyLayout implements Journa
 
     return formlayout;
   }
+
 
   @Override
   public void setParameter(BeforeEvent beforeEvent, Long id) {
